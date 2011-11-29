68ae10aa03ddf29358b1236994626e405399bace
Domon <domon@techbang.com.tw>
2011-11-29 12:27:38 +0800

    Add breadcrumb to Search#index

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,5 +3,9 @@ class SearchController < ApplicationController
   def index
     @topics = Topic.all
     @pages = Page.all
+
+    drop_breadcrumb("#{t("common.search")}")
+
+    render :stream => true
   end
 end
